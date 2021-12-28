074afca75b09001af7b0e923aaebcd64a08d47b1
Claudia <claudia.berrocalgomez@gmail.com>
2021-12-28 13:10:09 +0100

    add server request

--- js/main.js
+++ js/main.js
@@ -1,2 +1,20 @@
 'use strict';
 
+const searchText = document.querySelector('.searchtext');
+const searchBtn = document.querySelector('.searchbtn');
+const animeList = document.querySelector('.animelist');
+
+function handleSearchBtn(event) {
+    event.preventDefault();
+
+    fetch(`https://api.jikan.moe/v3/search/anime?q=${searchText.value}`)
+        .then((response) => response.json())
+        .then((data) => {
+            let results = data.results
+            for (let eachResult of results) {
+                animeList.innerHTML += `<li><img src="${eachResult.image_url}" alt="${eachResult.title}"><h2>${eachResult.title}</h2></li>`
+            }
+        })
+}
+
+searchBtn.addEventListener('click', handleSearchBtn)
