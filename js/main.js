457afa77fac0244a055c3f894dd807108c5b86ee
Claudia <claudia.berrocalgomez@gmail.com>
2021-12-29 18:36:48 +0100

    add button delate fav

--- js/main.js
+++ js/main.js
@@ -4,8 +4,10 @@ const searchText = document.querySelector('.searchtext');
 const searchBtn = document.querySelector('.searchbtn');
 const animeList = document.querySelector('.animelist');
 const favorites = document.querySelector('.favorites');
+const favsInLocal = document.querySelector('.favsinlocal');
 let allResults = [];
 let favoriteAnimes = [];
+let favoritesInLocal = [];
 
 function handleSearchBtn(event) {
     event.preventDefault();
@@ -26,7 +28,7 @@ function handleSearchBtn(event) {
 function paintResults() {
     animeList.innerHTML = '';
     for (let eachResult of allResults) {
-        if (eachResult.image_url === null) {
+        if (eachResult.image_url === 'https://cdn.myanimelist.net/images/qm_50.gif?s=e1ff92a46db617cb83bfc1e205aff620') {
             animeList.innerHTML += `<li class="anime" id="${eachResult.mal_id}"><img src="https://via.placeholder.com/210x295/567891/891234/?text=${eachResult.type}" alt="${eachResult.title}"><h2>${eachResult.title}</h2></li>`
         } else {
             animeList.innerHTML += `<li class="anime" id="${eachResult.mal_id}"><img src="${eachResult.image_url}" alt="${eachResult.title}"><h2>${eachResult.title}</h2></li>`
@@ -59,15 +61,16 @@ function getFavorite() {
         function handleAddfavorites(event) {
             let selectedAnime = event.currentTarget;
             if (eachAnime.style.backgroundColor === 'black') {
-                favoriteAnimes.push(selectedAnime.innerHTML);
+                favoriteAnimes.push(`<li>${selectedAnime.innerHTML}<button>Eliminar de favoritos</button></li>`);
             } else {
                 const indexFav = favoriteAnimes.indexOf(`${selectedAnime.innerHTML}`);
                 favoriteAnimes.splice(indexFav, 1);
             }
 
-            favorites.innerHTML = favoriteAnimes;
+            favorites.innerHTML = `${favoriteAnimes}`;
 
-            localStorage.setItem('favs', JSON.stringify(favorites.innerHTML))
+            localStorage.setItem('favs', JSON.stringify(favoriteAnimes))
+
         }
 
         eachAnime.addEventListener('click', handleAddfavorites);
@@ -75,9 +78,11 @@ function getFavorite() {
     };
 }
 
-const savedFavs = JSON.parse(localStorage.getItem('favs'))
+const savedFavs = JSON.parse(localStorage.getItem('favs'));
 
-favorites.innerHTML = savedFavs;
+favoritesInLocal = savedFavs;
+
+favsInLocal.innerHTML = favoritesInLocal;
 
 searchBtn.addEventListener('click', handleSearchBtn);
 
